3c322ccca8110556ba35d01eb8ada10e14dea78b
Student <example@example.com>
2019-06-05 13:47:35 -0700

    show all bikes stolen in a city

--- src/main.js
+++ src/main.js
@@ -10,8 +10,17 @@ $(document).ready(function() {
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-      $('.showSerial').text(`The serial in ${city} is ${body.bikes.serial}%`);
-      $('.showYear').text(`The year  is ${body.bikes.year} `);
+      var date = new Date(body.bikes[0].date_stolen * 1000);
+
+
+      for (let i=0; i<body.bikes.length; i++)
+      {
+          $('.showTitle').append("<li>"+ body.bikes[i].frame_colors + "</li>");
+
+
+    }
+
+
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
--- src/stolenbike.js
+++ src/stolenbike.js
@@ -2,7 +2,7 @@ export class StolenBike {
   findStolenBike(city) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = 'https://bikeindex.org/api/v3/search?page=1&per_page=25&location=&distance=10&stolenness=stolen';
+      let url = 'https://cors-anywhere.herokuapp.com/https://bikeindex.org/api/v3/search?page=1&per_page=25&location='+city+'&distance=10&stolenness=stolen';
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
